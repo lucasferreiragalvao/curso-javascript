247ea3c441ee9bd6e00ee3bf70bc097ee68f94d3
Lucas Galvão <lucasferreiragalvaoti@gmail.com>
2018-07-25 20:36:23 -0300

    Calculando IMC e validando informações

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,2 +1,30 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoEValido = true;
+var alturaEValida = true;
+
+if (peso <= 0 || peso >= 1000) {
+    console.log("Peso inválido!");
+    pesoEValido = false;
+    tdImc.textContent = "Peso inválido!";
+}
+
+if (altura <= 0 || altura >= 3.00) {
+    console.log("Altura inválida!");
+    alturaEValida = false;
+    tdImc.textContent = "Altura inválida";
+}
+if (pesoEValido && alturaEValida) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
