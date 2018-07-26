c6af69443be41688a4fb540e39ab340a7a5e4594
Lucas Galvão <lucasferreiragalvaoti@gmail.com>
2018-07-26 13:44:08 -0300

    Loop e estilizações

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,30 +1,37 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
 
-var tdImc = paciente.querySelector(".info-imc");
+for (var i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-var pesoEValido = true;
-var alturaEValida = true;
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-if (peso <= 0 || peso >= 1000) {
-    console.log("Peso inválido!");
-    pesoEValido = false;
-    tdImc.textContent = "Peso inválido!";
-}
+    var tdImc = paciente.querySelector(".info-imc");
 
-if (altura <= 0 || altura >= 3.00) {
-    console.log("Altura inválida!");
-    alturaEValida = false;
-    tdImc.textContent = "Altura inválida";
-}
-if (pesoEValido && alturaEValida) {
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
+    var pesoEValido = true;
+    var alturaEValida = true;
+
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido!");
+        pesoEValido = false;
+        tdImc.textContent = "Peso inválido!";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (altura <= 0 || altura >= 3.00) {
+        console.log("Altura inválida!");
+        alturaEValida = false;
+        tdImc.textContent = "Altura inválida";
+        paciente.classList.add("paciente-invalido");
+    }
+    if (pesoEValido && alturaEValida) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
 }
