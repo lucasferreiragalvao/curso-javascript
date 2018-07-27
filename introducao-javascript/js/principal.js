55146a1bf452e01b3d462eb0ba0c1d8010d4663d
Lucas Galvão <lucasferreiragalvaoti@gmail.com>
2018-07-26 21:46:26 -0300

    Tabela Dinamica Javascript

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -35,3 +35,40 @@ for (var i = 0; i < pacientes.length; i++) {
         tdImc.textContent = imc.toFixed(2);
     }
 }
+
+var botaoAddPaciente = document.querySelector("#adicionar-paciente");
+
+botaoAddPaciente.addEventListener("click",function(event){
+    event.preventDefault();
+    
+    var formAddPaciente = document.querySelector("#form-adicionar-paciente");
+    
+    var nome = formAddPaciente.nome.value;
+    var peso = formAddPaciente.peso.value;
+    var altura = formAddPaciente.altura.value;
+    var gordura = formAddPaciente.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    nomeTd.textContent = nome;
+
+    var pesoTd = document.createElement("td");
+    pesoTd.textContent = peso;
+
+    var alturaTd = document.createElement("td");
+    alturaTd.textContent = altura;
+
+    var gorduraTd = document.createElement("td");
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd)
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabelaPacientes = document.querySelector("#tabela-pacientes");
+
+    tabelaPacientes.appendChild(pacienteTr);
+
+});
